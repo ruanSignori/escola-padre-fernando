b2cd5b9583ea03052644168e81938c091549b3e4
ruanSignori <cleosignori@gmail.com>
2023-05-29 07:02:35 -0300

    style: changed background color

--- components/SwitchTheme/index.tsx
+++ components/SwitchTheme/index.tsx
@@ -19,7 +19,7 @@ export function SwitchTheme({ ...rest }: HTMLAttributes<HTMLDivElement>) {
 
   return (
     <div {...rest}>
-      <div className="max-w-4xl flex items-center justify-center gap-2 bg-gray-100 dark:bg-gray-800 py-3 px-5 rounded-xl transition-colors">
+      <div className="max-w-4xl flex items-center justify-center gap-2 bg-gray-100 dark:bg-zinc-700 py-3 px-5 rounded-xl transition-colors">
         <Switch
           onChange={() => setDarkTheme(!darkTheme)}
           checked={darkTheme}
