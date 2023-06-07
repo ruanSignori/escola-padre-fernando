f747cb25ad19f192309a04f6ee37e0e11509680c
ruanSignori <cleosignori@gmail.com>
2023-06-07 07:18:48 -0300

    feat: add new route projetos

--- pages/projetos/[name].tsx
+++ pages/projetos/[name].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function Projects() {
+export default function Projetos() {
   const router = useRouter();
   console.log(router);
   return <h1>ola</h1>;
