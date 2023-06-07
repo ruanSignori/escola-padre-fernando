58039813ebbf09a2b8884215385e487ce44bec8b
ruanSignori <cleosignori@gmail.com>
2023-06-07 07:23:44 -0300

    feat: inserted tab container

--- pages/projects/magesiel/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Magesiel() {
-  return <h1>Opa</h1>;
-}
---
+++ pages/projetos/magesiel/index.tsx
@@ -0,0 +1,13 @@
+import { TabContainer } from "@/components/TabContainer";
+import Image from "next/image";
+
+export default function Magesiel() {
+  return (
+    <div className=" py-20 md:py-32 px-3 min-h-screen bg-gray-100 dark:bg-black ">
+      <div className="flex flex-col flex-shrink max-w-6xl m-auto">
+        <h1 className="h1 text-light-blue-500">MageSiel</h1>
+        <TabContainer />
+      </div>
+    </div>
+  );
+}
