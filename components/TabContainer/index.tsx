66942803ddb34b7cbe638613785319f024e1b13e
ruanSignori <cleosignori@gmail.com>
2023-06-19 06:44:09 -0300

    :sparkles: feat: members rendering via API

--- components/TabContainer/index.tsx
+++ components/TabContainer/index.tsx
@@ -13,42 +13,142 @@ import {
   AcademicCapIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/solid";
+import { CardMembers } from "./CardMembers/Index";
+import { Members } from "@/models/members";
+import { useQuery } from "@tanstack/react-query";
+
+const getMembers = async (): Promise<Members[]> => {
+  const url = process.env.API_URL_MEMBERS;
+
+  if (!url) {
+    throw new Error("ApiUrl environment variable is undefined or invalid");
+  }
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Unable to access data, check URL");
+  }
+
+  return res.json();
+};
 
 export function TabContainer() {
-  const data = [
+  const { data } = useQuery({
+    queryKey: ["members"],
+    queryFn: getMembers,
+  });
+
+  const dataTab = [
     {
       label: "Home",
       value: "home",
       icon: HomeIcon,
-      desc: `O jogo MageSiel foi desenvolvido por alunos da Escola Estadual De Educação Básica Padre Fernando e pelo professor Igor dos Santos (Professor de Física e Matemática da escola). E atualmente você pode jogar o nosso jogo em qualquer sistema operacional para computador (não compatível com celulares).
-      O jogo é totalmente gratuito. Esperamos que você goste do jogo e se divirta. Se você for bom, talvez até consiga terminá-lo.
-      Boa sorte!`,
+      desc: (
+        <>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            O jogo MageSiel foi desenvolvido por alunos da{" "}
+            <strong>Escola Estadual De Educação Básica Padre Fernando</strong> e
+            pelo professor Igor dos Santos (Professor de Física e Matemática da
+            escola).
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            Atualmente você pode jogar o nosso jogo em qualquer sistema
+            operacional para computador (não compatível com celulares).
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            O jogo é totalmente gratuito. Esperamos que você goste do jogo e se
+            divirta. Se você for bom, talvez até consiga terminá-lo.
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            Boa sorte!
+          </Typography>
+        </>
+      ),
     },
     {
       label: "Desenvolvimento",
       value: "desenvolvimento",
       icon: ChartBarIcon,
-      desc: `O projeto “Game na escola” tem como foco principal apresentar uma experiência de desenvolvimento de games como ferramenta de aprendizagem.
-      Realizada com um grupo de alunos do 1º e 2° ano do ensino médio,o projeto procurou desenvolver um jogo de ação do tipo “plataforma 2D” elaborado desde a parte artística (desenhos e cenário feito pelos próprios alunos) até a parte da lógica de programação utilizada no jogo e na construção do site (de autoria do aluno do 2° ano Ruan Signori) conteúdo este que faz parte da grade curricular do novo ensino médio.`,
+      desc: (
+        <>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            O projeto “Game na escola” tem como foco principal apresentar uma
+            experiência de desenvolvimento de games como ferramenta de
+            aprendizagem.
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            Realizada com um grupo de alunos do 1º e 2° ano do ensino médio,o
+            projeto procurou desenvolver um jogo de ação do tipo “plataforma 2D”
+            elaborado desde a parte artística (desenhos e cenário feito pelos
+            próprios alunos) até a parte da lógica de programação utilizada no
+            jogo e na construção do site (de autoria do aluno do 2° ano Ruan
+            Signori) conteúdo este que faz parte da grade curricular do novo
+            ensino médio.
+          </Typography>
+        </>
+      ),
     },
     {
       label: "Objetivos",
       value: "objetivos",
       icon: AcademicCapIcon,
-      desc: `“É preciso abrir os sistemas de ensino a novas ideias. Em vez da homogeneidade e da rigidez, a diferença e a mudança” (Nóvoa - 2009).
-      Assim, um grande desafio da escola é proporcionar diferentes interfaces entre as linguagens contemporâneas, de forma que os jovens possam viver e interagir nesse mundo globalizado. Instruindo-os sob os aspectos tecnológicos, éticos, humanos e culturais, para que possam exercer sua cidadania de forma plena e busquem o conhecimento com autonomia e esse foi o grande intuito do projeto.`,
+      desc: (
+        <>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            “É preciso abrir os sistemas de ensino a novas ideias. Em vez da
+            homogeneidade e da rigidez, a diferença e a mudança” (Nóvoa - 2009).
+          </Typography>
+          <Typography
+            variant="paragraph"
+            className="text-gray-800 dark:text-gray-50"
+          >
+            Assim, um grande desafio da escola é proporcionar diferentes
+            interfaces entre as linguagens contemporâneas, de forma que os
+            jovens possam viver e interagir nesse mundo globalizado.
+            Instruindo-os sob os aspectos tecnológicos, éticos, humanos e
+            culturais, para que possam exercer sua cidadania de forma plena e
+            busquem o conhecimento com autonomia e esse foi o grande intuito do
+            projeto.
+          </Typography>
+        </>
+      ),
     },
     {
       label: "Créditos",
       value: "creditos",
       icon: UserGroupIcon,
-      desc: ``,
+      desc: data?.map(({ name, participant, id }, index) => (
+        <CardMembers name={name} participant={participant} key={index} />
+      )),
     },
   ];
+
   return (
     <Tabs value="home" className="mt-10">
       <TabsHeader className="dark:bg-gray-400">
-        {data.map(({ label, value, icon }) => (
+        {dataTab.map(({ label, value, icon }) => (
           <Tab key={value} value={value}>
             <div className="flex items-center gap-2 ">
               {React.createElement(icon, {
@@ -60,14 +160,9 @@ export function TabContainer() {
         ))}
       </TabsHeader>
       <TabsBody>
-        {data.map(({ value, desc }) => (
+        {dataTab.map(({ value, desc }) => (
           <TabPanel key={value} value={value}>
-            <Typography
-              variant="paragraph"
-              className="text-gray-800 dark:text-gray-50"
-            >
-              {desc}
-            </Typography>
+            {desc}
           </TabPanel>
         ))}
       </TabsBody>
--- components/TabContainer/types/TabContainerProps.ts
+++
@@ -1,5 +0,0 @@
-import { Members } from "@/models/members";
-
-export type TabContainerProps = {
-  members: Members[];
-};
