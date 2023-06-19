53d1b24c6bf53fdef37c06d0a3babd7fbde637ee
ruanSignori <cleosignori@gmail.com>
2023-06-19 06:36:32 -0300

    :sparkles: feat: new provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import "@/styles/index.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@material-tailwind/react";
 import { NavContainer } from "@/components/NavContainer";
+import { queryClient } from "@/services/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <NavContainer />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <NavContainer />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
