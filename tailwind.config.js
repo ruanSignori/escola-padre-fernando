511620f8a1f67c04e21ad19884d299581972bc90
ruanSignori <cleosignori@gmail.com>
2023-08-09 11:07:08 -0300

    fix: cannot find module

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/src/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/src/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
